02b586cfefbac5bbd39b822eeea345bb913eeb6a
Anastasiya Solovyova <rabtsavaas@gmail.com>
2024-04-30 01:02:06 +0300

    Initial commit

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Logo = () => {
+    return (
+        <div>
+            <span>Vahid Navazan</span>
+        </div>
+    );
+};
+
---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from "styled-components";
+import photo from '../../../assets/images/girl.webp'
+import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Button} from "../../../components/Button";
+export const Main = () => {
+    return (
+        <div>
+            <FlexWrapper>
+                <div>
+                    <MainTitle>Software Developer</MainTitle>
+                    <Name>Hello, my name is Vahid Navazan</Name>
+                    <span>Short text with details about you, what you do or your professional career. You can add more information on the about page.</span>
+                    <Button>Projects</Button>
+                    <Button>LinkedIn</Button>
+                </div>
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
+        </div>
+    );
+};
+
+const Photo = styled.img`
+    width: 720px;
+    height: 629px;
+    object-fit: cover;
+    
+`
+
+const MainTitle = styled.h1`
+
+`
+
+const Name = styled.h2`
+
+`
+
