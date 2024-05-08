da01639bfa9c1ef641e862b68e8f37176d3a7f3d
Anastasiya Solovyova <rabtsavaas@gmail.com>
2024-05-09 00:08:04 +0300

    css contact

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
+import {Container} from "../../../components/Container";
 
 export const Contact = () => {
     return (
         <StyledContact>
-            <SectionTitle>Contact Me</SectionTitle>
-            <StyledForm>
-                <Title>Name</Title>
-                <Field />
-                <Title>Email</Title>
-                <Field />
-                <Title>Message</Title>
-                <Field as={"textarea"}/>
-                <Button type={"submit"}>Send</Button>
-            </StyledForm>
+            <Container>
+                <SectionTitle>Contact Me</SectionTitle>
+                <StyledForm>
+                    <Title>Name</Title>
+                    <Field />
+                    <Title>Email</Title>
+                    <Field />
+                    <Title>Message</Title>
+                    <Field as={"textarea"}/>
+                    <Button type={"submit"}>Send</Button>
+                </StyledForm>
+            </Container>
         </StyledContact>
     );
 };
 
 const StyledContact= styled.section`
-    min-height: 50vh;
+    
 `
 
 const StyledForm = styled.form`
@@ -30,17 +33,42 @@ const StyledForm = styled.form`
     flex-direction: column;
     gap: 10px;
     margin: 0 auto;
+    align-items: center;
+    
+    
+    textarea {
+        width: 400px;
+        height: 160px;
+        resize: none;
+    }
     
 `
 const Field = styled.input`
-
+    position: static;
+    width: 400px;
+    height: 40px;
+    align-items: center;
 `
 const Button = styled.a`
-    
+    width: 89px;
+    height: 43px;
+    border-radius: 8px;
+    background: rgb(253, 196, 53);
+    padding: 8px 24px 8px 24px;
+   margin-left: 320px;
+    color: rgb(37, 40, 43);
+    font-family: Roboto;
+    font-size: 18px;
+    font-weight: 500;
+    text-align: left;
+   
 `
 
 const Title = styled.h2`
-    font-weight: 600;
+    color: rgb(37, 40, 43);
+    font-family: 'Nunito';
     font-size: 16px;
-    color: #25282b;
+    font-weight: 600;
+    line-height: 22px;
+    text-align: left;
 `
